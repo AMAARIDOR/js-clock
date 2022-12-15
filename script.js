9118ac37b940a28abc1bec3e74d86e4975b6896f
HZZRD <arish2k3@gmail.com>
2022-12-15 16:34:50 +0530

    clock now works perfectly except the intervals are unoptimized

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 "use strict";
 
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
 function currentTime() {
   let date = new Date();
   let hh = date.getHours();
@@ -13,4 +28,34 @@ function currentTime() {
   document.getElementById("clock").textContent = `${hh}:${mm}:${ss} hours`;
 }
 
+function currentDate() {
+  let date = new Date();
+  let day = date.getDate();
+  let month = date.getMonth();
+  let year = date.getFullYear();
+
+  let realMonth = months[month];
+  let realDay = day;
+
+  switch (day % 10) {
+    case 1:
+      realDay = `${day}st`;
+      break;
+    case 2:
+      realDay = `${day}nd`;
+      break;
+    case 3:
+      realDay = `${day}rd`;
+      break;
+    default:
+      realDay = `${day}th`;
+      break;
+  }
+
+  document.getElementById(
+    "date"
+  ).textContent = `${realDay} ${realMonth}, ${year}`;
+}
+
 setInterval(currentTime, 1000);
+setInterval(currentDate, 1000);
