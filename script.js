50ad16060dbb1c449f348f0719ee329364b4c0ef
HZZRD <arish2k3@gmail.com>
2022-12-15 16:05:36 +0530

    clock works with hours

--- script.js
+++ script.js
@@ -9,7 +9,8 @@ function currentTime() {
   ss < 10 ? (ss = `0${ss}`) : ss;
   mm < 10 ? (mm = `0${mm}`) : mm;
   hh < 10 ? (hh = `0${hh}`) : hh;
-  document.getElementById("clock").textContent = `${hh}:${mm}:${ss}`;
+
+  document.getElementById("clock").textContent = `${hh}:${mm}:${ss} hours`;
 }
 
 setInterval(currentTime, 1000);
