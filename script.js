9983ec59f94c6ff804f947c7cd7c12a6cd4e0caa
HZZRD <arish2k3@gmail.com>
2022-12-15 16:02:14 +0530

    clock works but without H in military form

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ function currentTime() {
   let hh = date.getHours();
   let mm = date.getMinutes();
   let ss = date.getSeconds();
+
+  ss < 10 ? (ss = `0${ss}`) : ss;
+  mm < 10 ? (mm = `0${mm}`) : mm;
+  hh < 10 ? (hh = `0${hh}`) : hh;
   document.getElementById("clock").textContent = `${hh}:${mm}:${ss}`;
 }
 
